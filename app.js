50d2690d415fb38cb800fa037d56078652d3e5ed
mrdonuzz <50772489+mrdonuzz@users.noreply.github.com>
2025-04-14 22:17:45 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+//1
+const box1 = document.querySelector("#box1");
+gsap.to(box1, {
+  duration: 1,
+  x: 300,
+  repeat: -1,
+});
+
+//2
+const box2 = document.querySelector("#box2");
+gsap.from(box2, {
+  duration: 1.5,
+  scale: 0,
+  opacity: 0,
+  repeat: -1,
+});
+
+//3
+const box3 = document.querySelector("#box3");
+gsap.fromTo(
+  box3,
+  {
+    scale: 2,
+    opacity: 0,
+  },
+  {
+    scale: 1,
+    opacity: 1,
+    duration: 2,
+    repeat: -1,
+  }
+);
